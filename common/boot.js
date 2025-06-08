81c9e768582d229e175002256102d3f5ceb8f2f6
LAPTOP-H4O8NVGG\User <chairozyrozy@gmail.com>
2025-06-09 06:00:30 +0700

    first commit

---
+++ common/boot.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2014 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Load the correct language pack and code bundle.
+ * @author blocklygames@neil.fraser.name (Neil Fraser)
+ */
+'use strict';
+
+
+(function() {
+  // Application path.
+  var appName = 'index';
+
+  var lang = 'en';
+  window['BlocklyGamesLang'] = lang;
+  window['BlocklyGamesLanguages'] = [lang];
+
+  // Load the chosen language pack.
+  var script = document.createElement('script');
+  script.src = 'lang/' + lang + '.js';
+  script.type = 'text/javascript';
+  document.head.appendChild(script);
+  // Load the code bundle for the chosen game.
+  var script = document.createElement('script');
+  script.src = appName + '/generated/uncompressed.js';
+  script.type = 'text/javascript';
+  document.head.appendChild(script);
+})();
